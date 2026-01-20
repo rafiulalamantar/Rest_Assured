7435c7ab229089a080568374db28568dc21f1802
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-01-20 23:10:37 +0600

    integrating the Multiple API's with common Json response value

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,19 +1,42 @@
 import files.payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.openqa.selenium.json.Json;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class Basics {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Add Place API which add places
 
-        RestAssured.baseURI= "https://rahulshettyacademy.com";
-       given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        Response response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
                 .body(payload.addPlace()).when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP"))
-                .header("server","Apache/2.4.52 (Ubuntu)");
+                .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("server", "Apache/2.4.52 (Ubuntu)").extract().response();
+        System.out.println(response.asString());
+
+        String responseString = response.asString();
+        JsonPath jsonPath = new JsonPath(responseString);
+        String placeId = jsonPath.getString("place_id");
+        System.out.println(placeId);
+
+        given()
+                .log().all()
+                .queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
+                .body("{ \"place_id\":\"" + placeId + "\", \"address\":\"70 winter walk, USA\", \"key\":\"qaclick123\" }")
+                .when()
+                .put("maps/api/place/update/json")
+                .then()
+                .assertThat()
+                .log().all()
+                .statusCode(200)
+                .body("msg", equalTo("Address successfully updated"));
+
 
     }
 
