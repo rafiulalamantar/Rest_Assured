7df196c41d924e3946cea5f40712c532f30196e0
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-01-20 22:32:15 +0600

    Assertions on Json Response body using JsonPath through Automated code

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,31 +1,20 @@
+import files.payload;
 import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Basics {
 
     public static void main(String[] args){
+        //Add Place API which add places
+
         RestAssured.baseURI= "https://rahulshettyacademy.com";
-        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}").when().post("maps/api/place/add/json")
-                .then().assertThat().statusCode(200);
+       given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+                .body(payload.addPlace()).when().post("maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP"))
+                .header("server","Apache/2.4.52 (Ubuntu)");
 
     }
-
 
 }
