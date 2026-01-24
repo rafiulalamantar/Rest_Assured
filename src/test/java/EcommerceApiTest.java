1f8bdeaea4612774a5e2db887ce37f6d5b720775
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2026-01-24 18:26:12 +0600

    create rest assured automation for login call to generate auth token using spec builder and pojo class

---
+++ src/test/java/EcommerceApiTest.java
@@ -0,0 +1,32 @@
+import com.google.common.base.Verify;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+import pojo.LoginRequest;
+import pojo.LoginResponse;
+
+import static io.restassured.RestAssured.*;
+    public class EcommerceApiTest {
+
+        @Test(description = "erify that a user can login successfully via E-Commerce API using valid credentials and retrieve token and userId")
+        public void ecommerceApiTest(){
+
+            RequestSpecification requestSpecification = new RequestSpecBuilder()
+                    .setBaseUri("https://rahulshettyacademy.com")
+                    .setContentType(ContentType.JSON)
+                    .build();
+            LoginRequest loginRequest = new LoginRequest();
+            loginRequest.setUserEmail("rahulshetty@gmail.com");
+            loginRequest.setUserPassword("Iamking@000");
+
+            RequestSpecification reqLogin = given().log().all().spec(requestSpecification)
+                    .body(loginRequest);
+            LoginResponse loginResponse = reqLogin
+                    .when().post("/api/ecom/auth/login")
+                    .then().log().all().extract().response().as(LoginResponse.class);
+            System.out.println(loginResponse.getToken());
+            System.out.println(loginResponse.getUserId());
+
+        }
+    }
